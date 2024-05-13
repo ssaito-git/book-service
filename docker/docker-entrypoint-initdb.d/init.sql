a7788088090f9a97e5c92ec8aae280746673c6a7
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-05-14 07:05:22 +0900

    ローカル開発環境用の Docker Compose を追加

---
+++ docker/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,13 @@
+create database book_service encoding 'UTF8';
+
+\c book_service;
+
+create schema book_service_schema;
+
+create user book_service_user password 'secret';
+
+grant usage on schema book_service_schema to book_service_user;
+
+alter default privileges in schema book_service_schema grant all on tables to book_service_user;
+
+alter user book_service_user set search_path to book_service_schema;
