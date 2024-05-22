91f3edba64a8a1634a3fdfbd708d03752853f3f8
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-05-22 19:52:17 +0900

    テーブルに created_at, updated_at を追加

--- book-service-db/src/main/resources/db/migration/V1.0.0__create_table.sql
+++ book-service-db/src/main/resources/db/migration/V1.0.0__create_table.sql
@@ -1,3 +1,12 @@
+-- updated_at の更新トリガー
+create function book_service_schema.update_updated_at() returns trigger as $$
+begin
+    new.updated_at = clock_timestamp();
+    return new;
+end;
+$$
+language plpgsql;
+
 -- 書籍
 create table book_service_schema.books
 (
@@ -5,8 +14,16 @@ create table book_service_schema.books
     author_id      UUID         not null,
     title          varchar(100) not null,
     title_kana     varchar(100) not null,
-    publisher_name varchar(100) not null
+    publisher_name varchar(100) not null,
+    created_at     timestamptz  not null default clock_timestamp(),
+    updated_at     timestamptz  not null default clock_timestamp()
 );
+
+create trigger update_books_updated_at
+    before update
+    on book_service_schema.books
+    for each row
+    execute procedure book_service_schema.update_updated_at();
 
 -- 著者
 create table book_service_schema.authors
@@ -15,5 +32,13 @@ create table book_service_schema.authors
     name       varchar(100) not null,
     name_kana  varchar(100) not null,
     birth_date date,
-    death_date date
+    death_date date,
+    created_at timestamptz  not null default clock_timestamp(),
+    updated_at timestamptz  not null default clock_timestamp()
 );
+
+create trigger update_authors_updated_at
+    before update
+    on book_service_schema.authors
+    for each row
+    execute procedure book_service_schema.update_updated_at();
